26d392c7ff73efbc42a35d7acbcff5cb11b6a6cb
Ylixandr <savoshinsky@inbox.ru>
2023-05-15 14:12:38 +0300

    Добавьте файлы проекта.

---
+++ Task_6/Program.cs
@@ -0,0 +1,36 @@
+﻿static int FindNearestGreater(int n)
+{
+    List<int> digits = new List<int>();
+    int temp = n;
+    while (temp > 0)
+    {
+        digits.Add(temp % 10);
+        temp /= 10;
+    }
+    digits.Reverse();
+    int[] arr = digits.ToArray();
+    for (int i = arr.Length - 2; i >= 0; i--)
+    {
+        Array.Sort(arr, i + 1, arr.Length - i - 1);
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                int temp1 = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp1;
+                Array.Sort(arr, i + 1, arr.Length - i - 1);
+                int result = 0;
+                foreach (int digit in arr)
+                {
+                    result = result * 10 + digit;
+                }
+                return result;
+            }
+        }
+    }
+    return 0;
+}
+
+int n = 2017;
+Console.WriteLine(FindNearestGreater(n));
